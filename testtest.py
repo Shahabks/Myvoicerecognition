77328b26a33ae40e12990a543bdb5d73ae224448
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2019-01-11 17:05:07 +0900

    Add files via upload

---
+++ testtest.py
@@ -0,0 +1,24 @@
+import mysprosody as mysp
+
+p="Walkers"
+c=r"C:\Users\ta\Desktop\mysprosodydir"
+
+
+mysp.mysptotal(p,c)
+mysp.myspgend(p,c)
+mysp.myspsyl(p,c)
+mysp.mysppaus(p,c)
+mysp.myspsr(p,c)
+mysp.myspatc(p,c)
+mysp.myspst(p,c)
+mysp.myspod(p,c)
+mysp.myspbala(p,c)
+mysp.myspf0mean(p,c)
+mysp.myspf0sd(p,c)
+mysp.myspf0med(p,c)
+mysp.myspf0min(p,c)
+mysp.myspf0max(p,c)
+mysp.myspf0q25(p,c)
+mysp.myspf0q75(p,c)
+mysp.mysppron(p,c)
+mysp.myprosody(p,c)
